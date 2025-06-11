4a777f04faeeec8ccc1fd3d22a65e90b7be00523
KeranLuciel <121261528+KeranLuciel@users.noreply.github.com>
2025-06-11 10:24:25 -0500

    Update tecnologia.php
    cambios en campos

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Gobierno prioriza inversión en ciencia, tecnología e innovación",
+"autor" => "Agencia Andina",
+"resumen" => "El Gobierno de Perú incrementará la inversión en ciencia, tecnología e innovación en el presupuesto público de 2025, destinando 238,6 millones de soles al Consejo Nacional de Ciencia, Tecnología e Innovación (Concytec). Las actividades prioritarias incluyen promover la integridad científica, apoyar a los Institutos Públicos de Investigación (IPI), descentralizar la CTI y articular con el sector académico e industrial.",
 ];
 ?>

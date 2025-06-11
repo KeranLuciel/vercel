7a16d5d848a8a22745de9a9e5c58fb034ef86719
KeranLuciel <121261528+KeranLuciel@users.noreply.github.com>
2025-06-11 10:25:14 -0500

    Update ciencia.php
    ciencia cambios en campos

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Perú incrementa inversión en ciencia, tecnología e innovación",
+"autor" => "Infobae",
+"resumen" => "El Gobierno de Perú ha incrementado la inversión en ciencia, tecnología e innovación en el presupuesto público de 2025, destinando 238,6 millones de soles al Consejo Nacional de Ciencia, Tecnología e Innovación (Concytec). Este incremento busca fortalecer el papel del Concytec como ente rector del Sistema Nacional de Ciencia, Tecnología e Innovación (Sinacti). Las intervenciones incluyen la promoción de la integridad científica, el apoyo a los Institutos Públicos de Investigación (IPI), y la descentralización de las actividades de ciencia, tecnología e innovación en el país.",
 ];
 ?>

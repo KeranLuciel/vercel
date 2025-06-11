d7d52cb3af1d39828ac7977c958bce56e6186f39
KeranLuciel <121261528+KeranLuciel@users.noreply.github.com>
2025-06-11 10:23:30 -0500

    Update economia.php
    cambio datos xx

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Telefónica del Perú cambia su nombre a Integratel Perú",
+"autor" => "El País",
+"resumen" => "Telefónica del Perú ha cambiado su nombre a Integratel Perú tras su venta en abril de 2025 a la empresa argentina Integra Tec International por 3,7 millones de soles y la asunción de una deuda de 1.200 millones de euros. La Junta General de Accionistas aprobó no sólo el cambio de denominación sino también la emisión de una nueva clase de acciones preferenciales sin voto. La empresa sigue operando sus servicios de telecomunicaciones para más de 13 millones de clientes en Perú, bajo la nueva administración. Las autoridades han constituido garantías por 102 millones de dólares para asegurar la participación en el proceso de adjudicación de espectro 5G. Paralelamente, Indecopi inició un procedimiento concursal ordinario para la reestructuración de la empresa, debido a embargos fiscales por más de 990 millones de soles y pérdidas acumuladas que superan un tercio del capital social pagado.",
 ];
 ?>

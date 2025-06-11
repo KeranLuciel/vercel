13ac2d007742923a6d9ae2cb3f51b8f8efd914f6
KeranLuciel <121261528+KeranLuciel@users.noreply.github.com>
2025-06-11 10:26:11 -0500

    Update cultura.php
    cambios en su contenido

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Vivo × el Rock 2025: regreso triunfal del festival",
+"autor" => "Wikipedia",
+"resumen" => "El festival Vivo × el Rock 2025 se llevó a cabo el sábado 29 de marzo de 2025 en Lurín Live, marcando el regreso del evento después de seis años de ausencia. Con una alineación destacada de 46 bandas nacionales e internacionales, el festival reunió a 35,000 personas para una jornada inolvidable.",
 ];
 ?>

22f965bb851ca141b56fdcf8f49482da3da843a1
KeranLuciel <121261528+KeranLuciel@users.noreply.github.com>
2025-06-11 10:22:17 -0500

    cambios portada.php
    contenido por nuevo

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada",
-"autor" => "jperez",
-"resumen" => "El resumen de resumenes",
+"titulo" => "Perú en junio de 2025: desafíos y avances",
+"autor" => "kramirez",
+"resumen" => "En junio de 2025, Perú enfrenta retos significativos en diversos ámbitos, desde la economía hasta la cultura. La reciente reestructuración de Telefónica del Perú, ahora Integratel Perú, marca un hito en el sector de telecomunicaciones. En el ámbito deportivo, la selección de fútbol empató con Ecuador, asegurando su clasificación al Mundial 2026. Además, el gobierno ha incrementado la inversión en ciencia, tecnología e innovación, destinando más de 238 millones de soles para fortalecer el ecosistema científico nacional.",
 ];
 ?>
